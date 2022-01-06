315e339722f9c9c55023998ea437862aa3d43f98
Ashu <ash.kelkar.poly@gmail.com>
2022-01-06 19:14:38 +0530

    firstapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Greet from './component/Greet';
+import React, { Component } from 'react';
+class App extends Component {
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="Bruce" heroname="batman" />
+      <Greet name="clark" heroname="superman" />
+      <Greet name="daina" heroname="wonder Woman" />
+      
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/component/Greet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+const Greet = props => {
+    console.log(props)
+    return (
+        <div>
+            <h1>
+            Hello {props.name} a.k.a {props.heroName}
+            <button>press Button</button>
+            </h1>
+        </div>
+    )
+
+}
+export default Greet
